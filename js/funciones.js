b0189fa66af51322a21c556cb8770f8ef4816dd7
IsMa <itorre13@hotmail.com>
2024-07-18 10:23:09 +0200

    sangusin_rutas_senderismo_80%

--- js/funciones.js
+++ js/funciones.js
@@ -66,40 +66,40 @@ if(headerSecundario){
 
 /* FUNCIONES MODAL GALERIA */
 
-const enlacesImagen = document.querySelectorAll(".galeria a");
-const modal = document.querySelector(".modal_galeria");
-const imgModal = document.querySelector(".modal_galeria img");
-const flechas = document.querySelectorAll(".modal_galeria button");
-const cerrarModal = document.querySelector(".boton_cerrar");
-let indiceImg = 0;
+const enlacesImagen = document.querySelectorAll(".galeria a"); // constante para seleccionar los elementos enlace dentro del elemento con la clase .galeria
+const modal = document.querySelector(".modal_galeria");  // constante para seleccionar los elementos con la clase .modal_galeria
+const imgModal = document.querySelector(".modal_galeria img"); // constante para seleccionar el elemento imagen dentro del elemento con la clase .modal_galeria
+const flechas = document.querySelectorAll(".modal_galeria button"); // constante para seleccionar los elementos botón dentro del elemento con la clase .modal_galeria
+const cerrarModal = document.querySelector(".boton_cerrar"); // constante para seleccionar los elementos con la clase .boton_cerrar
+let indiceImg = 0; // variable para crear un contador que tenga valor 0
 
 if(enlacesImagen){
-    enlacesImagen.forEach((enlaceImg,i) => {
-        enlaceImg.addEventListener("click", evento => {
-            evento.preventDefault();
-            indiceImg = i;
-            imgModal.setAttribute("src",enlaceImg.getAttribute("href"));
-            modal.classList.add("visible");
+    enlacesImagen.forEach((enlaceImg,i) => { // por cada uno de los enlaces realizará una función
+        enlaceImg.addEventListener("click", evento => { // al hacerle clik a ese enlace realizará el siguiente evento
+            evento.preventDefault(); // está función hará que no haga lo que por defecto tiene que hacer
+            indiceImg = i; // el contador tenga el valor del indice de los enlaces
+            imgModal.setAttribute("src",enlaceImg.getAttribute("href")); // cambiará al atributo src de la imagen por el del atributo href del enlace seleccionado
+            modal.classList.add("visible"); // añadirá la clase .visible
         });
     });
 }
 
 if(cerrarModal){
-    cerrarModal.addEventListener("click", () => {
-        modal.classList.remove("visible");
+    cerrarModal.addEventListener("click", () => { // al hacerle click a ese botón realizará el siguiente evento
+        modal.classList.remove("visible"); // elimina la clase .visible
     });
 }
 
 if(flechas){
     flechas.forEach((flecha,i) => {
-        flecha.addEventListener("click", (evento) => {
-            evento.stopPropagation();
-            if(i == 0){
-                indiceImg = indiceImg > 0 ? indiceImg - 1 : enlacesImagen.length - 1;
-            }else{
-                indiceImg = indiceImg < enlacesImagen.length - 1 ? indiceImg + 1: 0;
+        flecha.addEventListener("click", (evento) => { // al hacerle click a ese botón flecha realizará el siguiente evento
+            evento.stopPropagation(); //interrumpe el bubbling y así no lee los otros ”click” al regresar al inicio del html
+            if(i == 0){ // si el indice es igual a 0
+                indiceImg = indiceImg > 0 ? indiceImg - 1 : enlacesImagen.length - 1; // si el contador es mayor que 0, resta 1 y sino el numero total de enlaces menos 1. Para pasar las imágenes hacia 'atrás'
+            }else{ // si el indice no es igual a 0 
+                indiceImg = indiceImg < enlacesImagen.length - 1 ? indiceImg + 1: 0; // si el contador es menor que el numero de enlaces menos 1 suma 1 y sino 0. Para pasar las imágenes hacia 'adelante'
             }
-            imgModal.setAttribute("src",enlacesImagen[indiceImg].getAttribute("href"));
+            imgModal.setAttribute("src",enlacesImagen[indiceImg].getAttribute("href")); // cambiará al atributo src de la imagen por el del atributo href del enlace seleccionado cada vez que hagamos click en los botones con el contador que parte de 0 como referencia
         });
     });
 }
@@ -107,21 +107,21 @@ if(flechas){
 
 /* FUNCIONES FORMUALARIO-CONSULTA ENVIADA */
 
-const enviarConsulta = document.querySelector('form input[type="submit"]');
-const consultaEnviada = document.querySelector(".modal_consulta");
-const formulario = document.querySelector("form");
-const cerrar = document.querySelector(".boton_cerrar");
+const enviarConsulta = document.querySelector('form input[type="submit"]');  // constante para seleccionar el elemento input con la clase .submit dentro de su type, dentro de un formulario.
+const consultaEnviada = document.querySelector(".modal_consulta");  // constante para seleccionar el elemento con la clase .modal_consulta
+const formulario = document.querySelector("form");  // constante para seleccionar el elemento formulario
+const cerrar = document.querySelector(".boton_cerrar");  // constante para seleccionar el elemento con la clase .boton_cerrar
 
 if(enviarConsulta){
-    enviarConsulta.addEventListener("click", evento => {
-        evento.preventDefault();
-        consultaEnviada.classList.add("visible");
+    enviarConsulta.addEventListener("click", evento => { // al hacerle click a ese botón flecha realizará el siguiente evento
+        evento.preventDefault(); // está función hará que no haga lo que por defecto tiene que hacer
+        consultaEnviada.classList.add("visible"); // añadirá la clase .visible
         formulario.reset(); // resetea todo el formulario y vacía los placeholder (función aplicada de tutorial w3schools)
     });
 }
 
 if(cerrar){
-    cerrar.addEventListener("click", () => {
-        consultaEnviada.classList.remove("visible");
+    cerrar.addEventListener("click", () => { // al hacerle click a ese botón realizará el siguiente evento
+        consultaEnviada.classList.remove("visible"); // elimina la clase .visible
     });
 }
